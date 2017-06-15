907a66bb00ef519bd01e5278d3906ac519d1a2e1
manvedu <mavelandiad@gmail.com>
2017-06-15 11:20:10 -0500

    Update configuration

--- config.js
+++ config.js
@@ -1,12 +1,8 @@
 module.exports = {
-  consumer_key: 'TMaAGoP7xfFylCQvQuEQgkxTB',  
-  consumer_secret: 'unhJixuyuGJVJwaAUxfQDd7e7XgTz17Hc6CMdZVI14NYb8jSYs',
-  access_token: '3846507209-e5xVzSfVheumk68pCPSN5TtzPMz38W30PsKS5nx',
-  access_token_secret: '6dkXOnrMwr7Uwb9BuqECKe491SNDF1CgfnI5MSVWYAXvp'
-/*
-  access_token: '55665074-t8ro09lKJxi4zOEULQzW67B2b9yjdzmGfejIHVFHD',
-  access_token_secret: 'OL4gxM9WWFEE2kgYe0aABXyugMzPmrEhBA5dQFbF9rFEE'
-*/
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
 }
 
 
